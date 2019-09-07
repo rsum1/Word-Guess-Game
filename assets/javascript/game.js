5245f8340a10d96a2f65a691ee1b28304a32e657
rsum1 <ryanjsumpter+github@gmail.com>
2019-09-07 13:32:09 -0700

    complete game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,62 @@
+//array of words
 const words = ["Apple", "Banana", "Orange"]
 
+//gets new random word
 const getRandWord = function () {
   return words[Math.floor(Math.random() * words.length)].toLowerCase()
 }
 
+//initialize values
 let wins = 0
 let losses = 0
 let guesses = 10
-const lettersGuessed = []
+let lettersGuessed = []
 let word = getRandWord()
 
+//resets to initial state
+const reset = function () {
+  word = getRandWord()
+  lettersGuessed = []
+  guesses = 10
+  displayWord()
+  document.getElementById('guesses').textContent = guesses
+  document.getElementById('wins').textContent = wins
+  document.getElementById('losses').textContent = losses
+  document.getElementById('letters').textContent = lettersGuessed.join(', ')
+}
 
+//displays word with blanks
 const displayWord = function () {
+
+  //string of letters and blanks
   let wordStr = ""
+
+  //toggle tracks if user won
   let winStatus = true
+
+  //loop over word and build string
   word.split("").forEach(function (letter) {
+    //if letter is in word AND user guesses letter
     if (lettersGuessed.indexOf(letter) != -1) {
       wordStr += `${letter} `
     } else {
+      //add blanks for letters guessed
       wordStr += "_ "
+      //blank indicates game is not won
       winStatus = false
     }
   })
-  
+
+  //display current state of word string
   document.getElementById('word').textContent = wordStr
+
+  //if no blanks were added
+  if (winStatus) {
+    alert('Win!')
+    wins++
+    //reset game
+    reset()
+  }
 }
 
 document.onkeyup = function (event) {
@@ -35,8 +68,14 @@ document.onkeyup = function (event) {
         displayWord()
       } else {
         guesses--
-
+        document.getElementById('guesses').textContent = guesses
+        if (guesses <= 0) {
+          alert('Looser')
+          losses++
+          reset()
+        }
       }
     }
   }
 }
+displayWord()
