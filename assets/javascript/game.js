725e892804e22552dbd3e82854325582a41b7a48
rsum1 <ryanjsumpter+github@gmail.com>
2019-09-06 21:41:40 -0700

    displays correct letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,37 @@
 const words = ["Apple", "Banana", "Orange"]
 
+const getRandWord = function () {
+  return words[Math.floor(Math.random() * words.length)].toLowerCase()
+}
+
 let wins = 0
 let losses = 0
 let guesses = 10
 const lettersGuessed = []
+let word = getRandWord()
 
 
-const getRandWord = function () {
-  return words[Math.floor(Math.random() * words.length)]
-}
-
-const displayWord = function () {
-  const word = getRandWord()
+const displayWord = function (chosen) {
   let wordStr = ""
   word.split("").forEach(function (letter) {
-    wordStr += "_ "
+    if (letter === chosen || lettersGuessed.indexOf(letter) != -1) {
+      wordStr += `${letter} `
+    } else {
+      wordStr += "_ "
+    }
   })
 
   document.getElementById('word').textContent = wordStr
+}
+
+document.onkeyup = function (event) {
+  if (event.keyCode >= 65 && event.keyCode <= 90) {
+    if (word.includes(event.key)) {
+      lettersGuessed.push(event.key)
+      displayWord(event.key)
+      console.log(word)
+      console.log('yes')
+
+    }
+  }
 }
